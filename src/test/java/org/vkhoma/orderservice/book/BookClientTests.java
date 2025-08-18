cb69996040d196b910641048db432aeabdb276f5
vkhoma <16196348+HVV83@users.noreply.github.com>
2025-08-18 20:44:55 +0200

    feat(test): add BookClient integration tests with MockWebServer and update Gradle deps

---
+++ src/test/java/org/vkhoma/orderservice/book/BookClientTests.java
@@ -0,0 +1,77 @@
+package org.vkhoma.orderservice.book;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.io.IOException;
+
+class BookClientTests {
+
+    private MockWebServer mockWebServer;
+    private BookClient bookClient;
+
+    @BeforeEach
+    void setup() throws IOException {
+        this.mockWebServer = new MockWebServer();
+        this.mockWebServer.start();
+
+        var webClient = WebClient.builder()
+                .baseUrl(mockWebServer.url("/").uri().toString())
+                .build();
+        this.bookClient = new BookClient(webClient);
+    }
+
+    @Test
+    void whenBookExistsThenReturnBook() {
+        var bookIsbn = "1234567890";
+
+        var mockResponse = new MockResponse()
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setBody("""
+                      {
+                        "isbn": %s,
+                        "title": "Title",
+                        "author": "Author",
+                        "price": 9.90,
+                        "publisher": "Polarsophia"
+                      }
+                      """.formatted(bookIsbn));
+
+        mockWebServer.enqueue(mockResponse);
+
+        Mono<Book> book = bookClient.getBookByIsbn(bookIsbn);
+
+        StepVerifier.create(book)
+                .expectNextMatches(
+                        b -> b.isbn().equals(bookIsbn))
+                .verifyComplete();
+    }
+
+    @Test
+    void whenBookNotExistsThenReturnEmpty() {
+        String bookIsbn = "1234567891";
+
+        MockResponse mockResponse = new MockResponse()
+                .addHeader("Content-Type", "application/json; charset=utf-8")
+                .setResponseCode(404);
+
+        mockWebServer.enqueue(mockResponse);
+
+        StepVerifier.create(bookClient.getBookByIsbn(bookIsbn))
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+
+    @AfterEach
+    void clean() throws IOException {
+        this.mockWebServer.shutdown();
+    }
+}
