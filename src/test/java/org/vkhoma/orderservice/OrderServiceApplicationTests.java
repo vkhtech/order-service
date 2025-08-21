596f4df303e86bd81c0382ee14a4c19f5c03d96c
vkhoma <16196348+HVV83@users.noreply.github.com>
2025-08-21 21:10:48 +0200

    test: extended order service tests with message validation

--- src/test/java/org/vkhoma/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/org/vkhoma/orderservice/OrderServiceApplicationTests.java
@@ -1,8 +1,12 @@
 package org.vkhoma.orderservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -15,18 +19,28 @@ import org.vkhoma.orderservice.book.Book;
 import org.vkhoma.orderservice.book.BookClient;
 import org.vkhoma.orderservice.order.domain.Order;
 import org.vkhoma.orderservice.order.domain.OrderStatus;
+import org.vkhoma.orderservice.order.event.OrderAcceptedMessage;
 import org.vkhoma.orderservice.order.web.OrderRequest;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestChannelBinderConfiguration.class)
 @Testcontainers
 public class OrderServiceApplicationTests {
 
     @Container
     static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.12"));
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private OutputDestination output;
 
     @Autowired
     private WebTestClient webTestClient;
@@ -48,46 +62,51 @@ public class OrderServiceApplicationTests {
     }
 
     @Test
-    void whenGetOrdersThenReturn() {
-        String bookIsbn = "1234567893";
-        Book book = new Book(bookIsbn, "Title", "Author", 9.90);
-        given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.just(book));
-        OrderRequest orderRequest = new OrderRequest(bookIsbn, 1);
-        Order expectedOrder = webTestClient.post().uri("/orders")
-                .bodyValue(orderRequest)
-                .exchange()
-                .expectStatus().is2xxSuccessful()
-                .expectBody(Order.class).returnResult().getResponseBody();
-        assertThat(expectedOrder).isNotNull();
+	void whenGetOrdersThenReturn() throws IOException {
+		String bookIsbn = "1234567893";
+		Book book = new Book(bookIsbn, "Title", "Author", 9.90);
+		given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.just(book));
+		OrderRequest orderRequest = new OrderRequest(bookIsbn, 1);
+		Order expectedOrder = webTestClient.post().uri("/orders")
+				.bodyValue(orderRequest)
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(Order.class).returnResult().getResponseBody();
+		assertThat(expectedOrder).isNotNull();
+		assertThat(objectMapper.readValue(output.receive().getPayload(), OrderAcceptedMessage.class))
+				.isEqualTo(new OrderAcceptedMessage(expectedOrder.id()));
 
-        webTestClient.get().uri("/orders")
-                .exchange()
-                .expectStatus().is2xxSuccessful()
-                .expectBodyList(Order.class).value(orders -> {
-                    assertThat(orders.stream().filter(order -> order.bookIsbn().equals(bookIsbn)).findAny()).isNotEmpty();
-                });
-    }
+		webTestClient.get().uri("/orders")
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBodyList(Order.class).value(orders -> {
+					assertThat(orders.stream().filter(order -> order.bookIsbn().equals(bookIsbn)).findAny()).isNotEmpty();
+				});
+	}
 
     @Test
-    void whenPostRequestAndBookExistsThenOrderAccepted() {
-        String bookIsbn = "1234567899";
-        Book book = new Book(bookIsbn, "Title", "Author", 9.90);
-        given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.just(book));
-        OrderRequest orderRequest = new OrderRequest(bookIsbn, 3);
+	void whenPostRequestAndBookExistsThenOrderAccepted() throws IOException {
+		String bookIsbn = "1234567899";
+		Book book = new Book(bookIsbn, "Title", "Author", 9.90);
+		given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.just(book));
+		OrderRequest orderRequest = new OrderRequest(bookIsbn, 3);
 
-        Order createdOrder = webTestClient.post().uri("/orders")
-                .bodyValue(orderRequest)
-                .exchange()
-                .expectStatus().is2xxSuccessful()
-                .expectBody(Order.class).returnResult().getResponseBody();
+		Order createdOrder = webTestClient.post().uri("/orders")
+				.bodyValue(orderRequest)
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(Order.class).returnResult().getResponseBody();
 
-        assertThat(createdOrder).isNotNull();
-        assertThat(createdOrder.bookIsbn()).isEqualTo(orderRequest.isbn());
-        assertThat(createdOrder.quantity()).isEqualTo(orderRequest.quantity());
-        assertThat(createdOrder.bookName()).isEqualTo(book.title() + " - " + book.author());
-        assertThat(createdOrder.bookPrice()).isEqualTo(book.price());
-        assertThat(createdOrder.status()).isEqualTo(OrderStatus.ACCEPTED);
-    }
+		assertThat(createdOrder).isNotNull();
+		assertThat(createdOrder.bookIsbn()).isEqualTo(orderRequest.isbn());
+		assertThat(createdOrder.quantity()).isEqualTo(orderRequest.quantity());
+		assertThat(createdOrder.bookName()).isEqualTo(book.title() + " - " + book.author());
+		assertThat(createdOrder.bookPrice()).isEqualTo(book.price());
+		assertThat(createdOrder.status()).isEqualTo(OrderStatus.ACCEPTED);
+
+		assertThat(objectMapper.readValue(output.receive().getPayload(), OrderAcceptedMessage.class))
+				.isEqualTo(new OrderAcceptedMessage(createdOrder.id()));
+	}
 
     @Test
     void whenPostRequestAndBookNotExistsThenOrderRejected() {
