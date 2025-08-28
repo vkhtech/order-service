fbdcd9295d9bb04891c1f0d372ffa1b602511375
vkhoma <16196348+HVV83@users.noreply.github.com>
2025-08-28 16:23:33 +0200

    feat(web): add logging to OrderController

--- src/main/java/org/vkhoma/orderservice/order/web/OrderController.java
+++ src/main/java/org/vkhoma/orderservice/order/web/OrderController.java
@@ -1,5 +1,7 @@
 package org.vkhoma.orderservice.order.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,7 @@ import javax.validation.Valid;
 @RequestMapping("orders")
 public class OrderController {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
     private final OrderService orderService;
 
     public OrderController(OrderService orderService) {
@@ -28,11 +31,13 @@ public class OrderController {
     public Flux<Order> getAllOrders(
             @AuthenticationPrincipal Jwt jwt
     ) {
+        log.info("Fetching all orders");
         return orderService.getAllOrders(jwt.getSubject());
     }
 
     @PostMapping
     public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+        log.info("Order for {} copies of the book with ISBN {}", orderRequest.quantity(), orderRequest.isbn());
         return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
     }
 
